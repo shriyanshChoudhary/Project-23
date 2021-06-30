7db92ef237ca72ebd0753678bf7c43dd857c46b9
shriyanshChoudhary <85223834+shriyanshChoudhary@users.noreply.github.com>
2021-06-30 15:56:22 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic : true,
+            friction : 1
+        }
+        
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position // name spacing
+        fill("red");
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+
+    }
+
+}
